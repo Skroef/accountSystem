8e11c1c06cdba98b208f4b84a7b27fea750a7c5a
Ninno Hattingh <ninnohattingh.transformit@gmail.com>
2021-10-05 03:31:11 +0200

    Created the project from the begining. next video is DataBase Entities

--- accountSystem2/AccountSystemLogic/src/main/java/za/ac/nwu/ac/logic/flow/CreateAccountTypeFlow.java
+++
@@ -1,9 +0,0 @@
-package za.ac.nwu.ac.logic.flow;
-
-import za.ac.nwu.ac.domain.dto.AccountTypeDto;
-
-public interface CreateAccountTypeFlow {
-//    default AccountTypeDto create(AccountTypeDto accountTypeDto);
-
-    AccountTypeDto create(AccountTypeDto accountType);
-}
